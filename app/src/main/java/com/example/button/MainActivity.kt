7781452129533c91af77726137e4b8626ed6db0f
Loup_1234 <86883476+Loup-1234@users.noreply.github.com>
2024-04-07 14:54:51 +0200

    Button !!!

--- app/src/main/java/com/example/button/MainActivity.kt
+++ app/src/main/java/com/example/button/MainActivity.kt
@@ -1,20 +1,26 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.button
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.os.Vibrator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -41,20 +47,24 @@ fun MainScreen() {
     }
 
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.surface),
         contentAlignment = Alignment.Center
     ) {
         Button(
             modifier = Modifier.size(250.dp),
             shape = CircleShape,
             onClick = {
-                vibrator?.vibrate(100)
+                vibrator?.vibrate(64)
+                (context as Activity).finish()
             }
         ) {
             Image(
                 painter = painterResource(id = R.drawable.ic_launcher_foreground),
                 contentDescription = null,
                 modifier = Modifier.fillMaxSize(),
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.surface)
             )
         }
     }
